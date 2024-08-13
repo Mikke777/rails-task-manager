b3e90c14c962df015c642eb0654a90c69506d292
michele di stadio <distadiomichele93@gmail.com>
2024-08-13 14:30:20 +0200

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +8,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new", as: "new_task"
+  post "tasks", to: "tasks#create"
+  get "tasks/show/:id", to: "tasks#show", as: "task"
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
 end
